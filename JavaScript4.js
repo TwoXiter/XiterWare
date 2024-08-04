afceea0dc503348bae86c5a3e8eb869e4d725ad8
TwoXiter <xiterdev7@gmail.com>
2024-08-03 22:04:53 -0300

    Add files via upload

---
+++ JavaScript4.js
@@ -0,0 +1,10 @@
+$(function () {
+  ("use strict");
+
+  /* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+  setTimeout(function () {
+    $(".loader_bg").fadeToggle();
+  }, 1500);
+});
